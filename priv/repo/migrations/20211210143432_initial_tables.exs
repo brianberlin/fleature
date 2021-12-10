6c7638481f499fc1e499de131afc6c2052292a0d
Brian Berlin <bri@nberl.in>
2021-12-10 08:54:31 -0600

    initial tables

---
+++ priv/repo/migrations/20211210143432_initial_tables.exs
@@ -0,0 +1,29 @@
+defmodule Fleature.Repo.Migrations.InitialTables do
+  use Ecto.Migration
+
+  def change do
+    create(table(:organizations)) do
+      add(:name, :string)
+    end
+
+    create(table(:organizations_users)) do
+      add(:user_id, references(:users))
+      add(:organization_id, references(:organizations))
+    end
+
+    create(table(:projects)) do
+      add(:name, :string)
+      add(:organization_id, references(:organizations))
+    end
+
+    create(table(:environments)) do
+      add(:name, :string)
+      add(:project_id, references(:projects))
+    end
+
+    create(table(:feature_flags)) do
+      add(:name, :string)
+      add(:environment_id, references(:environments))
+    end
+  end
+end
